872a0dffffa546f935bcad1a266f0b798f038962
mchen16 <michael_chen@college.harvard.edu>
2017-06-02 20:37:14 -0400

    8:37 MC

--- mnist.py
+++ mnist.py
@@ -1,14 +1,9 @@
-from __future__ import division, print_function
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout Flatten
 from keras.layers.convolutional import *
-from keras.optimizers import SGD
 import numpy as np
 from keras.datasets import mnist
 from keras.utils.np_utils import to_categorical
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
-from keras.preprocessing.image import ImageDataGenerator
 
 def get_cnn(n_classes):
     model = Sequential()
@@ -55,20 +50,15 @@ batch_size = 1
 X_train = np.expand_dims(X_train, axis=3)
 X_test = np.expand_dims (X_test, axis=3)
 
-# Get training data for 0 and 1
+## Get training data for 0 and 1 ##
 inds = np.where((y_train == 0) | (y_train == 1))[0]
 
-X_train_small = X_train[inds]
-X_train_small = X_train_small[:train_size]
-y_train_small = y_train[inds]
-y_train_small = y_train_small[:train_size]
-
-# Get test data for 0 and 1 only
+## split into validation and test sets ##
 inds_test = np.where((y_test == 0) | (y_test == 1))
 X_test_01 = X_test[inds_test]
 y_test_01 = y_test[inds_test]
 
-# split into validation and test sets
+## split into validation and test sets ##
 test_start_ind = int(np.floor((len(X_test_01)/2)))
 X_test_small = X_test_01[:test_start_ind]
 X_final_test = X_test_01[test_start_ind:]
@@ -76,16 +66,18 @@ X_final_test = X_test_01[test_start_ind:]
 y_test_small = y_test_01[:test_start_ind]
 y_final_test = y_test_01[test_start_ind:]
 
-# one hot labels
+## One hot labels ##
 one_hot_train = to_categorical(y_train_small, 2)
 one_hot_test = to_categorical(y_test_small, 2)
 one_hot_final_test = to_categorical(y_final_test, 2)
 
-n_classes = one_hot_train.shape[1]
+n_classes = one_hot_test.shape[1]
 
 folds = 5
 evals = np.zeros((len(train_sizes*folds),3))
 index = 0
+
+## Training and evaluating CNN ##
 for train_size in train_sizes:
     for i in range(5):
         fold_inds = np.random.choice(inds,train_size)
@@ -109,6 +101,8 @@ X_final_test_flat = X_final_test.reshape((len(X_final_test),784))
 folds = 5
 evals = np.zeros((len(train_sizes*folds),3))
 index = 0
+
+## Training and evaluating multilayer perceptron ##
 for train_size in train_sizes:
     for i in range(5):
         fold_inds = np.random.choice(inds,train_size)
